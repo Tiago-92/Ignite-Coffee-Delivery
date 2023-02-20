6e7528a6ddaf5267c2c66464a290cb9db5844b32
Tiago <tiagoamaralcbei@hotmail.com>
2023-02-20 15:53:36 -0300

    finalizado título, subtítulo e imagem principal

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,26 +1,44 @@
 import { SectionContainer } from './styles'
 
 import cupCoffee from '../../assets/cup-coffee.png'
+import cartIcon from '../../assets/cart-icon.svg'
+import clockTimer from '../../assets/clock-timer.svg'
+import boxIcon from '../../assets/box-icon.svg'
+import coffeeIcon from '../../assets/coffee-icon.svg'
 
 export function Section() {
   return (
     <SectionContainer>
       <div className="title-subtitle">
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <h2>
+        <h1 className="title">
+          Encontre o café perfeito para qualquer hora do dia
+        </h1>
+        <h2 className="subtitle">
           Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
           hora
         </h2>
 
         <div className="list">
-          <span>Compra simples e segura</span>
-          <span>Embalagem mantém o café intacto</span>
-          <span>Entrega rápida e rastreada</span>
-          <span>O café chega fresquinho até você</span>
+          <span>
+            <img src={cartIcon} alt="ícone de um carrinho de compras" />
+            Compra simples e segura
+          </span>
+          <span className="item-right-1">
+            <img src={boxIcon} alt="ícone de uma caixa" />
+            Embalagem mantém o café intacto
+          </span>
+          <span>
+            <img src={clockTimer} alt="ícone de um relógio" />
+            Entrega rápida e rastreada
+          </span>
+          <span className="item-right-2">
+            <img src={coffeeIcon} alt="ícone de um copo de café" />O café chega
+            fresquinho até você
+          </span>
         </div>
       </div>
 
-      <div>
+      <div className="coffee-image">
         <img src={cupCoffee} alt="imagem de um copo de café" />
       </div>
     </SectionContainer>
--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -1,10 +1,51 @@
 import styled from 'styled-components'
 
 export const SectionContainer = styled.section`
+  @media (max-width: 700px) {
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+
+    margin: 0;
+    padding: 1.87rem;
+
+    .title-subtitle {
+      .title {
+        font-size: 2.19rem;
+        line-height: 2.5rem;
+        text-align: left;
+      }
+
+      .subtitle {
+        font-size: 1.12rem;
+      }
+
+      .list {
+        gap: 0.94rem;
+        width: 100%;
+
+        span {
+          margin: 0;
+        }
+      }
+    }
+
+    .coffee-image {
+      margin-top: 35px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
   display: flex;
+  gap: 3.5rem;
 
   margin-top: 5.87rem;
   margin-left: 10rem;
+  margin-right: 10rem;
 
   .title-subtitle {
     h1 {
@@ -33,6 +74,23 @@ export const SectionContainer = styled.section`
     margin-top: 4.12rem;
 
     width: 35.44rem;
-    height: 5.25rem;
+    height: 7rem;
+
+    span {
+      display: flex;
+      align-items: center;
+      gap: 0.75rem;
+
+      font-family: 'Roboto' sans-serif;
+      font-weight: 400;
+      line-height: 1.3rem;
+      font-size: 1rem;
+      color: ${(props) => props.theme['base-text']};
+    }
+
+    .item-right-1,
+    .item-right-2 {
+      margin-left: 2.5rem;
+    }
   }
 `
